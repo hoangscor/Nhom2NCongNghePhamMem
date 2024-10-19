5a3da9a623726a73a832065dc43a4218c610e443
hoangscor <hoanglk0001@gmail.com>
2024-10-19 19:23:40 +0700

    init new project added

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+using HairHarmonySalon.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HairHarmonySalon.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult index()
+        {
+            return View();
+        }
+        public IActionResult services()
+        {
+            return View(); 
+        }
+		public IActionResult gallery()
+		{
+			return View();
+		}
+		public IActionResult about()
+		{
+			return View();
+		}
+		public IActionResult blog()
+		{
+			return View();
+		}
+		public IActionResult contact()
+		{
+			return View();
+		}
+		public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        
+    }
+}
