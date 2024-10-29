c44a66d4fbc81f1341199ffb7844c4bc21461f06
hoangscor <hoanglk0001@gmail.com>
2024-10-29 12:51:20 +0700

    Them cai database

---
+++ Harmony.Repositories/Entities/Dashboard.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Harmony.Repositories.Entities;
+
+public partial class Dashboard
+{
+    public int DashboardId { get; set; }
+
+    public string? Statistics { get; set; }
+
+    public string? Reports { get; set; }
+}
---
+++ Harmony.Repositories/Entities/Stylist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Harmony.Repositories.Entities;
+
+public partial class Stylist
+{
+    public int StylistId { get; set; }
+
+    public string? SkillLevel { get; set; }
+
+    public string Availability { get; set; } = null!;
+
+    public virtual ICollection<Appointment> Appointments { get; set; } = new List<Appointment>();
+
+    public virtual SalonStaff StylistNavigation { get; set; } = null!;
+}
