8010209ecb100fd3f96bf46676161d456ebb9dcc
Lê Huy Hoàng <156869304+hoangscor@users.noreply.github.com>
2024-10-31 11:53:27 +0700

    crud cup dien

---
+++ Harmony.Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Harmony.Repositories.Entities;
+
+namespace Harmony.Repositories.Interfaces
+{
+	public interface IUserRepository
+	{
+		Task<List<User>> GetAllUser();
+
+	}
+}
---
+++ Harmony.Services/Interfaces/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using Harmony.Repositories.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Harmony.Repositories.Entities;
+
+namespace Harmony.Services.Interfaces
+{
+	internal interface IUserService
+	{
+		Task<List<User>> Users();
+	}
+}
